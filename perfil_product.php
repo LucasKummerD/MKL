30c3b804ed91798c6b359a0e173479644df6aa30
PatricioDominguez <dominguezalzaga@gmail.com>
2018-11-13 08:10:21 -0300

    Version 4.6 - Sprint3 sin terminar.

---
+++ perfil_product.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    <?php require 'header.php'; ?>
+
+    </head>
+    <body>
+    
+    <?php require 'navbar_admin.php'; ?> 
+
+    <?php //.... ?> 
+
+    <?php require 'footer.php'; ?> 
+    <?php require 'script.php'; ?> 
+
+    </body>
+</html>
